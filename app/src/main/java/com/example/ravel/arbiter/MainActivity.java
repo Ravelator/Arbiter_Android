5676a8bb5fefef13982d49c97287242d110da21e
Antoine <antoine.ravel@univ-tlse3.fr>
2018-04-11 16:57:27 +0200

    debut prog. camera

--- app/src/main/java/com/example/ravel/arbiter/MainActivity.java
+++ app/src/main/java/com/example/ravel/arbiter/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.ravel.arbiter;
 
 import android.content.Context;
-import android.content.Intent;
-import android.provider.MediaStore;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
+    public static final int MEDIA_TYPE_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,25 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         //Gets whether the selector wheel wraps when reaching the min/max value.
         np.setWrapSelectorWheel(true);
-/*
-        //Set a value change listener for NumberPicker
-        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                //Display the newly selected number from picker
-                Context context = getApplicationContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, newVal, duration);
-                toast.show();
-            }
-        });
-        */
 
         TextView textView = (TextView) findViewById(R.id.logs);
+        textView.setBackgroundColor(Color.BLACK);
+        textView.setTextColor(Color.WHITE);
         textView.setText("Init. du programme...");
-
 
 
         final Button boutonOk = (Button) findViewById(R.id.OKBouton);
@@ -55,7 +54,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 boutonOk.setEnabled(false);
-                prendrePhotos(np.getValue());
+                try {
+                    prendrePhotos(np.getValue());
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -70,23 +73,99 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void prendrePhotos(int freq) {
+    private void prendrePhotos(int freq) throws CameraAccessException {
 
         TextView texteView = findViewById(R.id.logs);
-        texteView.setText(texteView.getText()+"\nFrequence messages : toutes les "+freq+" secondes");
+        texteView.setText(texteView.getText() + "\nFrequence messages : toutes les " + freq + " secondes");
 
         Context context = getApplicationContext();
 
-        Toast toast = Toast.makeText(context,"Frequence messages : "+freq,Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(context, "Frequence messages : " + freq, Toast.LENGTH_SHORT);
         toast.show();
 
+        //On initialise la caméra -> on regarde si la caméra est accessible
 
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+                // this device has a camera
+                texteView.setText(texteView.getText() + "\nCaméra accessible : initialisation...");
+
+                Camera c = null;
+                try {
+                    c = Camera.open(); // attempt to get a Camera instance
+
+
+                }
+                catch (Exception e){
+                    // Camera is not available (in use or does not exist)
+                }
+
+
+            } else {
+                // no camera on this device
+                texteView.setText(texteView.getText() + "\nCaméra inaccessible");
             }
 
 
     }
 
+    /** A basic Camera preview class */
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+        private SurfaceHolder mHolder;
+        private Camera mCamera;
+
+        public CameraPreview(Context context, Camera camera) {
+            super(context);
+            mCamera = camera;
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+            // deprecated setting, but required on Android versions prior to 3.0
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        public void surfaceCreated(SurfaceHolder holder) {
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+                Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            }
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // empty. Take care of releasing the Camera preview in your activity.
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e){
+                Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            }
+        }
+    }
 }
