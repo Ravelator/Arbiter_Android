8e07a29b49f33ababb7cd38ef98ad0105d575f72
Antoine <antoine.ravel@univ-tlse3.fr>
2018-04-19 21:15:14 +0200

    Travail sur l'upload

---
+++ app/src/main/java/com/example/ravel/arbiter/UploadFile.java
@@ -0,0 +1,14 @@
+package com.example.ravel.arbiter;
+
+import android.os.AsyncTask;
+
+public class UploadFile extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return upload();
+    }
+
+    private Object upload() {
+        return null;
+    }
+}
